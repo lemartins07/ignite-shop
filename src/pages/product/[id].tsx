c195ea93af8f072a2988492dfba7ec9ded31847f
Leandro Martins <leandro.marttins.85@gmail.com>
2023-06-17 16:25:17 -0300

    Produto & Checkout - Trabalhando com SEO

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -7,6 +7,7 @@ import Stripe from "stripe";
 import { useRouter } from 'next/router';
 import { useState } from "react";
 import axios from "axios";
+import Head from "next/head";
 
 interface ProductProps {
   product: {
@@ -46,22 +47,27 @@ export default function Product({ product }: ProductProps) {
   }
 
   return (
-    <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} width={520} height={480} alt="" />
-      </ImageContainer>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        </ImageContainer>
 
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
 
-        <p>{product.description}</p>
+          <p>{product.description}</p>
 
-        <button onClick={handleBuyButton} disabled={isCreatingCheckoutSession}>
-          Comprar Agora
-        </button>
-      </ProductDetails>
-    </ProductContainer> 
+          <button onClick={handleBuyButton} disabled={isCreatingCheckoutSession}>
+            Comprar Agora
+          </button>
+        </ProductDetails>
+      </ProductContainer> 
+    </>
   )
 }
 
